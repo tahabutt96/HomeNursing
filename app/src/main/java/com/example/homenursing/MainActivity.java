f8ec3efb55e242993e377e0be9e62fbf494aa620
unknown <Arsalankhan051995@gmail.com>
2020-10-06 21:29:43 +0500

    Splash Screen

--- app/src/main/java/com/example/homenursing/MainActivity.java
+++ app/src/main/java/com/example/homenursing/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.homenursing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN= 5000;
+
+
+    //variables
+
+    Animation topanim, bottomanim;
+    ImageView image;
+    TextView logo,slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //animation
+
+        topanim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomanim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //hooks
+
+        image = findViewById(R.id.imageview);
+        logo = findViewById(R.id.textview);
+        slogan = findViewById(R.id.textview2);
+
+        image.setAnimation(topanim);
+        logo.setAnimation(bottomanim);
+        slogan.setAnimation(bottomanim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
+
+
+
+
+
+
